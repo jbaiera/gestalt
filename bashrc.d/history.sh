4a3867bfc5ad8bbe8b27766f13d61cca2678c628
James Baiera <james.baiera@gmail.com>
2015-10-13 18:05:52 -0400

    Porting over some other things for the environment

---
+++ bashrc.d/history.sh
@@ -0,0 +1,7 @@
+# history.sh
+
+# set the history counts
+export HISTSIZE=1000
+export HISTFILESIZE=1000
+export HISTCONTROL=ignoredups
+export HISTTIMEFORMAT="%Y-%m-%dT%T "
---
+++ bashrc.d/mesg.sh
@@ -0,0 +1,4 @@
+# mesg.sh
+
+# Set up to recieve no terminal messages
+mesg n
